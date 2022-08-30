2bb22d00b227194e8b47a8b9260598e8532fe986
vietanhkvh <vietanh.kvh@gmail.com>
2022-08-30 17:14:31 +0700

    build done

--- libs/ui-image-respondsive-size/src/lib/ui-image-respondsive-size.stories.tsx
+++ libs/ui-image-respondsive-size/src/lib/ui-image-respondsive-size.stories.tsx
@@ -1,14 +1,21 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { UiImageRespondsiveSize } from './ui-image-respondsive-size';
-
+import styles from './ui-image-respondsive-size.stories.module.scss';
 export default {
   component: UiImageRespondsiveSize,
   title: 'UiImageRespondsiveSize',
 } as ComponentMeta<typeof UiImageRespondsiveSize>;
 
 const Template: ComponentStory<typeof UiImageRespondsiveSize> = (args) => (
-  <UiImageRespondsiveSize {...args} />
+  <div className={styles['bigger-container']}>
+    <UiImageRespondsiveSize {...args} />
+  </div>
 );
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+  classnames: {
+    container: styles['container'],
+    content: styles['content'],
+  },
+};
--- libs/ui-image-respondsive-size/src/lib/ui-image-respondsive-size.tsx
+++ libs/ui-image-respondsive-size/src/lib/ui-image-respondsive-size.tsx
@@ -1,12 +1,24 @@
+import classNames from 'classnames';
 import styles from './ui-image-respondsive-size.module.scss';
 
 /* eslint-disable-next-line */
-export interface UiImageRespondsiveSizeProps {}
+interface IClassName {
+  container: string;
+  content: string;
+}
+export interface UiImageRespondsiveSizeProps {
+  classnames: IClassName;
+}
 
 export function UiImageRespondsiveSize(props: UiImageRespondsiveSizeProps) {
+  const { classnames = { container: '', wrapper: '', content: '' } } = props;
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to UiImageRespondsiveSize!</h1>
+    <div className={classNames(styles['img-container'], classnames.container)}>
+      <img
+        className={classNames(styles['img-content'], classnames.content)}
+        alt=""
+        src="https://upload.lixibox.com/system/banners/covers/000/001/269/original/1660560523.jpg"
+      />
     </div>
   );
 }
