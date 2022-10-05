11281585c2ce7e8ebc12e9d97400bb95c093e866
vietanhkvh <vietanh.kvh@gmail.com>
2022-10-05 09:26:22 +0700

    fix dot indicator is not appeare

--- libs/ui-antd/mobile/src/lib/swipes/swipes.stories.tsx
+++ libs/ui-antd/mobile/src/lib/swipes/swipes.stories.tsx
@@ -1,12 +1,41 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Swipes } from './swipes';
-
+import styles from './swipes.module.scss';
 export default {
   component: Swipes,
   title: 'UiAntd/Swipes',
 } as ComponentMeta<typeof Swipes>;
 
 const Template: ComponentStory<typeof Swipes> = (args) => <Swipes {...args} />;
-
+const indicatorNumber = (total: number, current: number) => (
+  <div className={styles.customIndicatorNum}>
+    {current + 1}/{total}
+  </div>
+);
+const indicatorCus = (total: number, current: number) => (
+  <div className={styles.customIndicator}></div>
+);
 export const Primary = Template.bind({});
 Primary.args = {};
+export const AutoPlay = Template.bind({});
+AutoPlay.args = { isAutoPlay: true, isLoop: true, trackOffset: 0 };
+export const Loop = Template.bind({});
+Loop.args = { isLoop: true };
+export const NotAllowTouch = Template.bind({});
+NotAllowTouch.args = { isAllowTouchMove: false };
+export const CustomStyle = Template.bind({});
+CustomStyle.args = { classnames: styles.border };
+export const NotFullWidth = Template.bind({});
+NotFullWidth.args = { slideSide: 80, trackOffset: 20, stuckAtBoundary: true };
+export const IndicatorIndex = Template.bind({});
+IndicatorIndex.args = {
+  isAutoPlay: true,
+  isLoop: true,
+  indicator: indicatorNumber,
+};
+export const IndicatorDotCustom = Template.bind({});
+IndicatorDotCustom.args = {
+  isAutoPlay: true,
+  isLoop: true,
+  indicator: indicatorCus,
+};
--- libs/ui-antd/mobile/src/lib/swipes/swipes.tsx
+++ libs/ui-antd/mobile/src/lib/swipes/swipes.tsx
@@ -1,5 +1,6 @@
 import styles from './swipes.module.scss';
-import { Button, Space, Swiper, Toast } from 'antd-mobile';
+import { Swiper, Toast } from 'antd-mobile';
+import { ReactNode } from 'react';
 const colors = ['#ace0ff', '#bcffdd', '#e4fabd', '#ffcfac'];
 const items = colors.map((c, i) => (
   <Swiper.Item>
@@ -15,13 +16,46 @@ const items = colors.map((c, i) => (
   </Swiper.Item>
 ));
 /* eslint-disable-next-line */
-export interface SwipesProps {}
+export interface SwipesProps {
+  isAutoPlay?: boolean;
+  isLoop?: boolean;
+  isAllowTouchMove?: boolean;
+  classnames?: string;
+  defaultIndexItem?: number;
+  slideSide?: number;
+  trackOffset?: number;
+  stuckAtBoundary?: boolean;
+  indicator?: (total: number, current: number) => ReactNode;
+  classnameIn?: string;
+}
 
 export function Swipes(props: SwipesProps) {
+  const {
+    isAutoPlay = false,
+    isLoop = false,
+    isAllowTouchMove = true,
+    classnames = '',
+    slideSide = 100,
+    trackOffset = 0,
+    stuckAtBoundary = false,
+    classnameIn = '',
+    indicator,
+  } = props;
   return (
     <div className={styles['container']}>
-      {/* <Swiper>{items}</Swiper> */}
-      <h1>dsfsd</h1>
+      <Swiper
+        autoplay={isAutoPlay}
+        allowTouchMove={isAllowTouchMove}
+        className={classnames}
+        loop={isLoop}
+        trackOffset={trackOffset}
+        slideSize={slideSide}
+        stuckAtBoundary={stuckAtBoundary}
+        indicator={indicator}
+        style={{ padding: '0 0 16px' }}
+      >
+        {items}
+      </Swiper>
     </div>
   );
 }
